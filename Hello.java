e0f95b5fbbf156a149d772a0984108fb24c99e1f
vikt2000 <108516562+vikt2000@users.noreply.github.com>
2023-09-11 15:49:37 +0200

    diller

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
     public static void main(String[] args) {  
-        var message = "Hello, World!";
+        var message = "Hello, World! gay gay gay";
         System.out.println(message);
 }
 }
